b2badf76015f4df568e820afea552b7c7de7fde2
pixelaria <info@pixelaria.ru>
2018-05-22 18:00:58 +0300

    footer informations

--- public_html/catalog/controller/common/footer.php
+++ public_html/catalog/controller/common/footer.php
@@ -3,30 +3,26 @@ class ControllerCommonFooter extends Controller {
 	public function index() {
 		$this->load->language('common/footer');
 
+		// Static links
+		$data['catalog'] = $this->url->link('product/category','category_id='.$this->config->get('config_main_category'),true);
+		$data['cart'] = $this->url->link('checkout/cart', '', true);
+		$data['contact'] = $this->url->link('information/contact');
+		$data['account'] = $this->url->link('account/account', '', true);
+		$data['special'] = $this->url->link('product/special');
+		$data['search'] = $this->url->link('product/search');
+
+		// Dynamic links
+
 		$this->load->model('catalog/information');
 
 		$data['informations'] = array();
 
-		foreach ($this->model_catalog_information->getInformations() as $result) {
-			if ($result['bottom']) {
-				$data['informations'][] = array(
-					'title' => $result['title'],
-					'href'  => $this->url->link('information/information', 'information_id=' . $result['information_id'])
-				);
-			}
+		foreach ($this->model_catalog_information->getFooterInformations() as $result) {
+			$data['informations'][] = array(
+				'title' => $result['title'],
+				'href'  => $this->url->link('information/information', 'information_id=' . $result['information_id'])
+			);
 		}
-
-		$data['contact'] = $this->url->link('information/contact');
-		$data['return'] = $this->url->link('account/return/add', '', true);
-		$data['sitemap'] = $this->url->link('information/sitemap');
-		$data['tracking'] = $this->url->link('information/tracking');
-		$data['manufacturer'] = $this->url->link('product/manufacturer');
-		$data['affiliate'] = $this->url->link('affiliate/login', '', true);
-		$data['special'] = $this->url->link('product/special');
-		$data['account'] = $this->url->link('account/account', '', true);
-		$data['order'] = $this->url->link('account/order', '', true);
-		$data['wishlist'] = $this->url->link('account/wishlist', '', true);
-		$data['newsletter'] = $this->url->link('account/newsletter', '', true);
 
 		$data['powered'] = sprintf($this->language->get('text_powered'), $this->config->get('config_name'), date('Y', time()));
 
